97bd1832f7665cb99c7c5ad187d50b50c5db57e7
Vharun <vharunsivaraj06@gmail.com>
2025-05-24 13:40:38 +0530

    run it

--- backEnd/controlers/loginControler.js
+++ backEnd/controlers/loginControler.js
@@ -1,5 +1,5 @@
 const LoginUserSchema = require("../model/LoginUserSchema")
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const secretkey = 'radhka06'
 
